562faa680e140f567d379c8ff8f5a89e3dd31f30
vhanregalado <vhanregalado@gmail.com>
2018-06-28 01:59:57 +0800

    recommit changes

--- application/models/Users_model.php
+++ application/models/Users_model.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Users_Model extends My_Model {
+class Users_Model extends MY_Model {
 
     function __construct() {
         parent::__construct();        
